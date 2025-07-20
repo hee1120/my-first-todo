8621c438c14c271c64c8df42722a9853529fd16c
hee1120 <fijuni1120@gmail.com>
2025-07-20 21:18:33 +0900

    feat: todo用のルーティングを追加

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,10 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
     route("about", "routes/about.tsx"),
+
+    // 一覧画面(作成と削除)
+    route("todos", "routes/todos.tsx", [
+        // 詳細画面(編集)
+        route(":id", "routes/todo.tsx"),
+    ]),
 ] satisfies RouteConfig;
