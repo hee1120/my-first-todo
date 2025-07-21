02691e3622f87fd20b558bb31d6b83092c697892
hee1120 <fijuni1120@gmail.com>
2025-07-21 09:25:01 +0900

    feat: 詳細画面のワイヤーフレームを実装

--- app/routes/todos/todo.tsx
+++ app/routes/todos/todo.tsx
@@ -1,7 +1,24 @@
+import { useLoaderData } from "react-router";
+import { TODOS } from "~/mock/todos";
+
+export const loader = ({params}: {params: {id: string}}) => {
+  const todo = TODOS.find((todo) => todo.id === Number(params.id));
+  return { todo };
+}
+
 const Todo = () => {
+  const { todo } = useLoaderData<typeof loader>();
   return (
     <div>
-      <h1>Todo Detail</h1>
+      <h2 className="text-lg font-bold">Detail</h2>
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-1">
+          <h3 className="text-md">{todo?.title}</h3>
+          <div className="text-gray-500 w-full">期日：{todo?.dueDate}</div>
+          <div className="text-gray-500 w-full">ステータス：{todo?.isDone ? "完了" : "未完了"}</div>
+        </div>
+        <div className="w-full">{todo?.description}</div>
+      </div>
     </div>
   )
 }
