f220d573e703873dd3554fe7d07abf96f8e0a6cd
hee1120 <fijuni1120@gmail.com>
2025-07-21 09:47:27 +0900

    feat: 編集フォームを実装

--- app/routes/todos/todo.tsx
+++ app/routes/todos/todo.tsx
@@ -33,48 +33,55 @@ const Todo = () => {
 
   return (
     <div className="border border-gray-200 p-4 rounded-md">
-      {isEditing ? (
-        <div>
-          <div className="flex justify-end gap-2">
-            <button
-              type="button"
-              className="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded-md"
-            >
-              Save
-            </button>
-            <button 
-              type="button"
-              onClick={handleCancelClick}
-              className="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded-md"
-            >
-              Cancel
-            </button>
-          </div>
-        </div>
-      ) : (
-        <>
-          <div className="flex justify-between">
-            {/* タイトル */}
-            <h2 className="text-lg font-bold">Detail</h2>
-            {/* 編集ボタン   */}
-            <button 
-              type="button"
-              onClick={handleEditClick}
-              className="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded-md" 
-            >
-              Edit
-            </button>
-          </div>
+      <div className="flex justify-between">
+        {/* タイトル */}
+        <h2 className="text-lg font-bold">Detail</h2>
+        {/* 編集ボタン   */}
+        {!isEditing && (
+          <button 
+            type="button"
+            onClick={handleEditClick}
+            className="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded-md" 
+          >
+            Edit
+          </button>
+        )}
+      </div>
+      <div className="flex flex-col gap-2">
+        {isEditing ? (
           <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-1">
+              <input type="text" className="border border-gray-200 p-2 rounded-md" value={formState.title} onChange={(e) => setFormState({...formState, title: e.target.value})} />
+              <input type="text" className="border border-gray-200 p-2 rounded-md" value={formState.dueDate} onChange={(e) => setFormState({...formState, dueDate: e.target.value})} />
+              <textarea className="border border-gray-200 p-2 rounded-md" value={formState.description} onChange={(e) => setFormState({...formState, description: e.target.value})} />
+            </div>
+            <div className="flex justify-end gap-2">
+              <button 
+                type="button"
+                onClick={handleCancelClick}
+                className="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded-md"
+              >
+                Cancel
+              </button>
+              <button
+                type="button"
+                className="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded-md"
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        ) : (
+          <>
             <div className="flex flex-col gap-1">
               <h3 className="text-md">{todo?.title}</h3>
               <div className="text-gray-500 w-full">期日：{todo?.dueDate}</div>
               <div className="text-gray-500 w-full">ステータス：{todo?.isDone ? "完了" : "未完了"}</div>
             </div>
             <div className="w-full">{todo?.description}</div>
-          </div>
-        </>
-      )}
+          </>
+        )}
+      </div>
     </div>
   )
 }
