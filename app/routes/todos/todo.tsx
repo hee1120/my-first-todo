5fff785ea342a820fe78c6556093c8ca0abd1544
hee1120 <fijuni1120@gmail.com>
2025-07-21 09:31:58 +0900

    refactor: 編集ボタンを追加

--- app/routes/todos/todo.tsx
+++ app/routes/todos/todo.tsx
@@ -9,8 +9,13 @@ export const loader = ({params}: {params: {id: string}}) => {
 const Todo = () => {
   const { todo } = useLoaderData<typeof loader>();
   return (
-    <div>
-      <h2 className="text-lg font-bold">Detail</h2>
+    <div className="border border-gray-200 p-4 rounded-md">
+      <div className="flex justify-between">
+        {/* タイトル */}
+        <h2 className="text-lg font-bold">Detail</h2>
+        {/* 編集ボタン   */}
+        <button type="button" className="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded-md">Edit</button>
+      </div>
       <div className="flex flex-col gap-2">
         <div className="flex flex-col gap-1">
           <h3 className="text-md">{todo?.title}</h3>
